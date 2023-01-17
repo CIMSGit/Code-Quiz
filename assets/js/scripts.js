5647cf2b42f2e4a41443d76acaa0fc9219a1a1f5
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-01-17 23:36:34 +0000

    adds  clearInterval to timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,33 +2,47 @@ let quizButton = document.getElementById("start");
 let timerEl = document.getElementById("time");
 let time =100;
 
+let startScreen =document.getElementsByClassName("start-screen");
+
 let quizQuestions = [{
 question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans1" },
 {
-question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" },
+question: "quest2", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" },
     {
-question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans3" },
+question: "quest3", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans3" },
         {
-question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" }
+question: "quest4", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" }
 ];
-
-
 
 
 
 // adds event listener to start quiz button and activates timer
 quizButton.addEventListener('click', function(event)
 {
-    
-    setInterval(function () {
+  
+
+ startScreen.hidden = true;
+
+let myInterval =setInterval(function () {
         time--;
-        timerEl.innerHTML =time}, 1000);
+        
+        timerEl.innerHTML =time;
+    
+    if (time <=0) {
+        clearInterval(myInterval)
+        timerEl.innerHTML ="Time is up!"
+        
+    }
+    
+    
+    }, 1000)
+     
+
+
+
+
 
 }
 
 )
-
-
-
-
 
