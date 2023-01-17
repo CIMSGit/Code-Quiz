646e1831889f6a6a9e1f7f79ec70049ca5b4d75f
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-01-17 22:13:14 +0000

    adds quiz questions and answers

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,22 @@
 let quizButton = document.getElementById("start");
 let timerEl = document.getElementById("time");
 let time =100;
+
+let quizQuestions = [{
+question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans1" },
+{
+question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" },
+    {
+question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans3" },
+        {
+question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" }
+];
+
+
+
+
+
+// adds event listener to start quiz button and activates timer
 quizButton.addEventListener('click', function(event)
 {
     
@@ -11,3 +27,8 @@ quizButton.addEventListener('click', function(event)
 }
 
 )
+
+
+
+
+
