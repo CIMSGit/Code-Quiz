b0e79da8d900a9111b2ff2e7f0552369ef366c87
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-01-18 02:24:23 +0000

    creates first round of question with answers using for loop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,48 +1,68 @@
 let quizButton = document.getElementById("start");
 let timerEl = document.getElementById("time");
-let time =100;
+let time = 100;
+let wrapper = document.querySelector("#start-screen");
 
-let startScreen =document.getElementsByClassName("start-screen");
+
+
+
 
 let quizQuestions = [{
-question: "quest1", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans1" },
+    question: "quest1", answers: ["ans1", "ans2", "ans3", "ans4", "ssss"], rightAnswer: "ans1"
+},
 {
-question: "quest2", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" },
-    {
-question: "quest3", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans3" },
-        {
-question: "quest4", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans2" }
+    question: "quest2", answers: ["ans1", "ans2", "ans3", "ans4"], rightAnswer: "ans2"
+},
+{
+    question: "quest3", answers: ["ans1", "ans2", "ans3"], rightAnswer: "ans3"
+},
+{
+    question: "quest4", answers: ["ans1", "ans2", "ans3", "ans4"], rightAnswer: "ans2"
+}
 ];
 
-
+let quiz = Object.values(quizQuestions);
 
 // adds event listener to start quiz button and activates timer
-quizButton.addEventListener('click', function(event)
-{
-  
+quizButton.addEventListener('click', function () {
 
- startScreen.hidden = true;
+    wrapper.style.display = "none";
 
-let myInterval =setInterval(function () {
+
+
+    let myInterval = setInterval(function () {
         time--;
-        
-        timerEl.innerHTML =time;
-    
-    if (time <=0) {
-        clearInterval(myInterval)
-        timerEl.innerHTML ="Time is up!"
-        
-    }
-    
-    
+
+        timerEl.innerHTML = time;
+
+        if (time <= 0) {
+            clearInterval(myInterval)
+            timerEl.innerHTML = "Time is up!"
+
+        }
+
+
     }, 1000)
-     
+
+
+    //the array
 
 
 
+    let p = document.createElement("p");
+    p.textContent =quiz[2].question;
+    document.body.appendChild(p);
+    for (let i = 0; i < quiz[2].answers.length; i++) {
+
+
+        let btn = document.createElement("button");
+        let wrapper = document.createTextNode(quiz[2].answers[i]);
+        btn.appendChild(wrapper);
+        wrapper = document.body.appendChild(btn);
+    }
 
 
 }
-
 )
+
 
